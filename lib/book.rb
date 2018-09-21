6d3a13f05c9fccc49a4ba297a26882b3b46e9f22
Irfan Suleman <ivsuleman@gmail.com>
2018-09-21 17:13:11 +0100

    fixed

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,41 @@
+class Book
+
+    def initialize(title)
+        @title = title
+    end
+
+    def title
+        @title
+    end
+
+    def author= var_author
+         @author = var_author
+    end
+
+    def author
+        @author
+    end
+
+    def page_count= var_page_count
+        @page_count = var_page_count
+    end
+
+    def page_count
+        @page_count
+    end
+
+    def genre
+        @genre
+    end
+
+    def genre= var_genre
+        @genre = var_genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
 
 
